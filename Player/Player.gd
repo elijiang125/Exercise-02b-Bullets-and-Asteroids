f49b609eee8d76dc4c59245b3756af47a058cd68
Elizabeth Jiang <elijiang@iu.edu>
2022-09-15 23:20:42 -0400

    Completed exercise

--- Player/Player.gd
+++ Player/Player.gd
@@ -3,10 +3,11 @@ extends KinematicBody2D
 var velocity = Vector2.ZERO
 
 var rotation_speed = 5.0
-var speed = 0.1
-var max_speed = 10
+var speed = 5
+var max_speed = 400
 
-
+onready var Bullet = load("res://Assets/Bullet.png")
+var nose = Vector2(0,-60)
 
 func _ready():
 	pass
@@ -14,15 +15,28 @@ func _ready():
 func _physics_process(_delta):
 	position = position + velocity
 	velocity = velocity.normalized() * clamp(velocity.length(), 0, max_speed)
+	velocity = move_and_slide(velocity,Vector2.ZERO)
 	
+	position.x = wrapf(position.x, 0, 1024)
+	position.y = wrapf(position.y, 0, 600)
+	
+	if Input.is_action_just_pressed("shoot"):
+		var Effects = get_node_or_null("/root/Game/Effects")
+		if Effects != null:
+			var bullet = Bullet.instance()
+			bullet.global_position = global_position + nose.rotated(rotation)
+			bullet.rotation = rotation
+			add_child(bullet)
+
+func get_input():
+	
+	var to_return = Vector2.ZERO
 	$Exhaust.hide()
 	if Input.is_action_pressed("forward"):
-		velocity = velocity + Vector2(0,-speed).rotated(rotation)
+		to_return.y -= to_return.y 
 		$Exhaust.show()
 	if Input.is_action_pressed("left"):
 		rotation_degrees = rotation_degrees - rotation_speed
-	if Input.is_action_pressed("right"):
+	if Input.is_action_pressed("right"): 
 		rotation_degrees = rotation_degrees + rotation_speed
-		
-	position.x = wrapf(position.x, 0, 1024)
-	position.y = wrapf(position.y, 0, 600)
+	return to_return.rotate(rotation)
